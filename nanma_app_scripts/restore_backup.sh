cd4992c9d59ab1828a39e3c07d4929eb9a6fe3ad
sajith k <sajithksasi@gmail.com>
2023-09-22 22:01:21 +0530

    updated restore path

--- nanma_app_scripts/restore_backup.sh
+++ nanma_app_scripts/restore_backup.sh
@@ -4,7 +4,7 @@ site_name=15.207.121.10
 db_backup=20230922_000037-3_108_85_75-database.sql.gz
 
 cd ../
-sudo docker cp nanma_apps_scripts/backup/$db_backup $(sudo docker compose ps -q backend):/tmp
+sudo docker cp nanma_app_scripts/backup/$db_backup $(sudo docker compose ps -q backend):/tmp
 sudo docker compose exec backend bench --site $site_name restore /tmp/$db_backup --mariadb-root-password $db_password
 sudo docker compose exec backend bench --site $site_name migrate
 sudo docker compose restart backend
