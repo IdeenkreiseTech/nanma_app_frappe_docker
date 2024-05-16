40fe25ee8605dc9a5ed69dc5edf44cf50c50e548
root <root@ip-172-31-21-17.ap-south-1.compute.internal>
2024-05-16 09:09:45 +0000

    added maintenence mode script to  download backup script

--- nanma_app_scripts/download_backup.sh
+++ nanma_app_scripts/download_backup.sh
@@ -3,6 +3,7 @@ db_password=123
 site_name=13.235.60.186
 
 cd ../
+# sudo docker compose exec backend bench --site $site_name set-maintenance-mode on
 sudo docker compose exec backend bench --site $site_name backup
 sudo docker cp $(sudo docker compose ps -q backend):/home/frappe/frappe-bench/sites/$site_name/private/backups/ nanma_app_scripts/
 
